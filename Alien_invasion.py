aff3cc827e2015fa55e4349f451406811371b8e9
HeperW <hgym300400@126.com>
2020-01-29 21:41:41 +0800

    yes

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -1,7 +1,8 @@
 import pygame
-
 from settings import Settings
-from ship import Ship 
+from ship import Ship
+import game_functions as gf 
+
 def run_game():
     #初始化游戏并创建一个游戏对象
     pygame.init()
@@ -17,15 +18,9 @@ def run_game():
     while True:
 
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-     
-        #每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
+        gf.check_events()
 
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        #每次循环时都重绘屏幕并让最近绘制的屏幕可见
+        gf.update_screen(ai_settings,screen,ship)
 
 run_game
